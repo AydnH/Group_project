fb4d94589aab2e28952aa26796fa34dd76741e7a
default <default@email.com>
2019-05-23 21:09:34 +1200

    Created payment page and images folder. Added to css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { MainHeader } from './components/navigation/MainHeader';
 import { HomePage } from './pages/Home/HomePage';
 import { RegisterPage } from './pages/Register/RegisterPage';
 import { MyProfilePage } from './pages/MyProfile/MyProfilePage';
+import { PaymentPage } from './pages/Payment/PaymentPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/my-profile" component={MyProfilePage} />
+		<Route path="/payment" component={PaymentPage} />
       </div>
     </HashRouter>
   );
