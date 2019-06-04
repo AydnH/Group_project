9b27855de09e9ecf808f520095c7077b16522ee0
Guest User <Guest@wg401-a-06-10.mddn.vuw.ac.nz>
2019-06-04 13:00:25 +1200

    merge into working file

--- app/Client/src/pages/SignUp/signUpPage.jsx
+++ app/Client/src/pages/SignUp/signUpPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { ButtonToolbar } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-
 
 export class SignUpPage extends React.Component {
   render() {
@@ -23,18 +21,14 @@ export class SignUpPage extends React.Component {
     
 
 	
-
- <button className= 'buttonStyle'><Link to='/RegisterPage'>SIGN UP</Link></button>
-
+ <button className= 'buttonStyle'><Link to='/Register'>SIGN UP</Link></button>
 
 
 <br/>
 <br/>
 <br/>
  
-
- <button className= 'buttonStyle2'><Link to='/RegisterPage'>LOGIN</Link></button>
-
+ <button className= 'buttonStyle2'><Link to='/Register'>LOGIN</Link></button>
 
 <br/>
 <br/>
@@ -42,18 +36,14 @@ export class SignUpPage extends React.Component {
 
 
 
-
-  <button className= 'buttonStyle3'><Link to='/RegisterPage'><img style={{width:75, height:17}} src={require('./images/facebook.png')} alt='Logo'/></Link></button>
-
-
+ <button className= 'buttonStyle3'><Link to='/Register'><img style={{width:75, height:17}} src={require('./images/facebook.png')} alt='Logo'/></Link></button>
 
 
 <br/>
 <br/>
 <br/>
 
-
-  <button className= 'buttonStyle4'><Link to='/RegisterPage'><img style={{width:20, height:20}} src={require('./images/google.png')} alt='Logo'/></Link></button>
+ <button className= 'buttonStyle4'><Link to='/Register'><img style={{width:20, height:20}} src={require('./images/google.png')} alt='Logo'/></Link></button>
 
 
 
