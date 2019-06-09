fbd1d1a82b1402bf3ead2458d18ba572bd28226c
Guest User <Guest@wg401-d-03-06.mddn.vuw.ac.nz>
2019-06-09 12:29:24 +1200

    live attempt 10

--- server.js
+++ server.js
@@ -23,20 +23,17 @@ mongoose.connect(dbRoute, { useNewUrlParser: true });
 let db = mongoose.connection;
 db.once('open', () => console.log('connected to the database'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// serve static assests if in production
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
+app.use(express.static(path.resolve('../client/build'), { index: false }));
 
-
-// if(process.env.NODE_ENV === 'production') {
-// 	//set static folder
-// 	app.use(express.static('client/build'));
-
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// 	});
-// }
+app.get('/*', (req, res, next) => {
+    res.sendFile('index.html', {
+      root: path.resolve('../client/build')
+    });
+  });
 
 
 router.get('/getData', (req, res) => {
