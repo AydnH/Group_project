0c241be440c25ac3a582e2af8f438d2df7b646d8
Guest User <Guest@wg401-i-07-07.mddn.vuw.ac.nz>
2019-06-10 18:13:59 +1200

    please work

--- server.js
+++ server.js
@@ -1,102 +1,90 @@
 const mongoose = require('mongoose');
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const User = require('./Models/User');
 const Data = require('./Models/Data');
 const Perms = require("./Models/Permissions");
 const Loco = require("./Models/Location");
-
-const API_PORT = 3001;
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-const router = express.Router()
-// const path = require();
+const path = require('path');;
 
 const dbRoute = 'mongodb+srv://Aydn:F1s2t7r9aw10%21@database-tsy14.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(dbRoute, { useNewUrlParser: true });
-
 
 let db = mongoose.connection;
 db.once('open', () => console.log('connected to the database'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger('dev'));
-app.use(express.static(path.resolve('../client/build'), { index: false }));
+const API_PORT = process.env.PORT || 3100;
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+const app = express()
+	.use(cors())
+	.use(express.json())
+	.use(express.urlencoded({ extended: true }))
+	.use(bodyParser.urlencoded({ extended: false }))
+	.use(bodyParser.json())
+	.use(logger('dev'));
 
+app
+	.use(express.static(path.resolve('./Client/build')));
+	
+const router = express.Router()
 
 router.get('/getData', (req, res) => {
 	User.find((err, user) => {
-		if (err) return res.json({ success: false, error: err});
-		return res.json({success: true, user:user})
+		if (err) return res.json({ success: false, error: err });
+		return res.json({ success: true, user: user })
 	});
 });
+
 router.get('/get-Data', (req, res) => {
 	Data.find((err, data) => {
-		if(err) return res.json({success: false, error:err});
-		return res.json({success: true, data:data})
+		if (err) return res.json({ success: false, error: err });
+		return res.json({ success: true, data: data })
 	});
 });
 
 router.get('/get-Location', (req, res) => {
 	Loco.find((err, loco) => {
-		if(err) return res.json({success: false, error:err});
-		return res.json({success: true, loco:loco})
+		if (err) return res.json({ success: false, error: err });
+		return res.json({ success: true, loco: loco })
 	});
 });
 
 router.get('/get-Permissions', (req, res) => {
 	Perms.find((err, perms) => {
-		if(err) return res.json({success: false, error:err});
-		return res.json({success: true, perms:perms})
+		if (err) return res.json({ success: false, error: err });
+		return res.json({ success: true, perms: perms })
 	});
 });
-
 
 //Register
-router.post('/putData', function(req, res){
-	User.create(req.body).then(function(user){
-		res.send(user);
+router.post('/putData', async (req, res) => {
+		const user = await
+		User.create(req.body);
 		console.log(user);
-		});
-});
-
+	});
 
 //SetUp
-router.post('/put-Data', function(req, res){
-	Data.create(req.body).then(function(data){
-		res.send(data);
+router.post('/put-Data', async (req, res) => {
+		const data = await
+		Data.create(req.body);
 		console.log(data);
 	});
-});
 
-router.post('/put-Location', function(req, res){
-	Loco.create(req.body).then(function(loco){
-		res.send(loco);
+router.post('/put-Location', async (req, res) => {
+		const loco = await
+		Loco.create(req.body);
 		console.log(loco);
-	});
-});
-router.post('/put-Permissions', function(req, res){
-	Perms.create(req.body).then(function(perms){
-		res.send(perms);
-		console.log(perms);
-	});
+	
 });
 
+router.post('/put-Permissions', async (req, res) => {
+		const perms = await
+		Perms.create(req.body);
+		console.log(perms);
+});
 
 app.use('/api', router);
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
